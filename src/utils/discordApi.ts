1f23e75c85d7bf38cbb3801c371286578acf1399
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-12-24 20:58:24 +0100

    fixed backend url

--- src/utils/discordApi.ts
+++ src/utils/discordApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export async function GetUserStatus() {
-  const { data } = await axios.get("http://localhost:3000/status");
+  const { data } = await axios.get("https://noobs-server-bot-help.herokuapp.com/status");
   return data;
 }
