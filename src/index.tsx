70e30aac536d53394dbd5f28fa78825f6eadd163
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-12-25 20:06:53 +0100

    remove the service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import ReactDOM from "react-dom"
 import { App } from "./App"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
   <App />,
   document.getElementById("root"),
 )
-serviceWorker.register()
