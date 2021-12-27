acb97b39adf04f8b295d0a01a7daefbfea21e76a
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-12-27 10:25:09 +0100

    fixed the tenary logic

--- src/components/main/_header/_header_status.tsx
+++ src/components/main/_header/_header_status.tsx
@@ -23,7 +23,7 @@ export function HeaderStatus() {
   return (
     <Box>
       {
-        currStatus ?
+        currStatus && currStatus.name && currStatus?.image ?
           <HeaderStatusBox data={currStatus} />
           : null
       }
