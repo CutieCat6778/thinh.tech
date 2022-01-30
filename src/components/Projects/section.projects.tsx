95801e39829a7a9381875ab8fb5e8a33cd05d660
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-01-30 10:30:17 +0100

    fixed projects

--- src/components/Projects/section.projects.tsx
+++ src/components/Projects/section.projects.tsx
@@ -6,45 +6,31 @@ import { WeatherNOW } from "./weatherNow.projects";
 
 export function SectionProjects() {
   return (
-    <Flex justifyContent="center" alignItems={"center"}>
+    <Flex
+      justifyContent="center"
+      alignItems={"center"}
+      _hover={{
+        marginTop: "-10px",
+        transition: "margin 2s",
+      }}
+    >
       <Grid
         templateColumns={{ base: "repeat(1, 2fr)", "2sm": "repeat(2, 3fr)" }}
         id="projects"
       >
-        <GridItem
-          _hover={{
-            marginTop: "-10px",
-            transition: "margin 2s",
-          }}
-        >
+        <GridItem>
           <Moddy />
         </GridItem>
         <GridItem
           ml={{ base: 0, "2sm": "50px" }}
           mt={{ base: "50px", "2sm": 0 }}
-          _hover={{
-            marginTop: "-10px",
-            transition: "margin 2s",
-          }}
         >
           <MentionConverter />
         </GridItem>
-        <GridItem
-          mt={"50px"}
-          _hover={{
-            marginTop: "40px",
-            transition: "margin 2s",
-          }}
-        >
+        <GridItem mt={"50px"}>
           <WeatherNOW />
         </GridItem>
-        <GridItem
-          mt={"50px"}
-          _hover={{
-            marginTop: "40px",
-            transition: "margin 2s",
-          }}
-        >
+        <GridItem mt={"50px"}>
           <Noobs />
         </GridItem>
       </Grid>
