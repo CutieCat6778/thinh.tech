512b65397af8db2cb3342868fa8a11e5799f9929
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-04-13 18:01:41 +0200

    added some other things

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import { register } from "./serviceWorker";
 
 ReactDOM.render(
     <App />,
     document.getElementById("root")
 );
+
+register();
