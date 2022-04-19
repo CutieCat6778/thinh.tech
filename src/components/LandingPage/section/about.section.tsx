86240393dc8edabec1aaa2bb67a80e7f40444c4d
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-04-19 21:53:45 +0200

    fixed image size and maybe perfomance

--- src/components/LandingPage/section/about.section.tsx
+++ src/components/LandingPage/section/about.section.tsx
@@ -8,7 +8,7 @@ export const MotionBox = motion(
     forwardRef<ChakraProps, "div">((props, ref) => {
         return (
             <Box ref={ref} {...props}>
-                <AboutBox/>
+                <AboutBox />
             </Box>
         );
     })
@@ -18,7 +18,7 @@ export function AboutBox() {
     return (
         <Flex justifyContent={"space-around"}
             alignItems="center"
-            flexDir={{ base: "column", "2lg": "row" }}
+            flexDir={{ base: "column", "lg": "row" }}
             width="100%"
             mb="5rem">
             <Flex flexDir={"column"} padding="3rem">
@@ -29,9 +29,25 @@ export function AboutBox() {
                     Since I was a kid, I&apos;ve been fascinated with technology. I can spend all day just learning about it. As I went to school, my passion for technology gained more momentum, and I am eager to develop my interest further.
                 </Text>
             </Flex>
-            <Image src="https://cutiecat6778.github.io/cdn/portfolio/about.webp"
-                width={{ base: "350px", "lg": "400px", "xl": "600px" }}
-                height={{ base: "230px", "lg": "290px", "xl": "430px" }}
+            <Image src={"https://cutiecat6778.github.io/cdn/portfolio/about_2mp.webp"}
+                display={{ base: "none", xl: "block" }}
+                width="800"
+                height={"500"}
+                alt="Profile picture"
+                borderRadius={"3%"}
+                boxShadow="2xl" />
+            <Image src={"https://cutiecat6778.github.io/cdn/portfolio/about_600_400.webp"}
+                display={{ base: "none", lg: "block", xl: "none" }}
+                alt="Profile picture"
+                borderRadius={"3%"}
+                boxShadow="2xl" />
+            <Image src={"https://cutiecat6778.github.io/cdn/portfolio/about_480_320.webp"}
+                display={{ base: "none", md: "block", lg: "none" }}
+                alt="Profile picture"
+                borderRadius={"3%"}
+                boxShadow="2xl" />
+            <Image src={"https://cutiecat6778.github.io/cdn/portfolio/about_320_233.webp"}
+                display={{ base: "block", md: "none"}}
                 alt="Profile picture"
                 borderRadius={"3%"}
                 boxShadow="2xl" />
