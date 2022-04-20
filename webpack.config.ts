3a3ee1f951e6d12026a5751d5c6bdbd8ceb80e71
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-04-20 10:11:02 +0200

    added webpack again

---
+++ webpack.config.ts
@@ -0,0 +1,91 @@
+import TerserPlugin from 'terser-webpack-plugin';
+import path from "path";
+import { Configuration, DefinePlugin } from "webpack";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as webpackDevServer from "webpack-dev-server";
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const config: Configuration = {
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "[name].bundle.js",
+        chunkFilename: "[name].chunk.js"
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "build"),
+        },
+        compress: true,
+        port: 3000,
+    },
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+        runtimeChunk: "multiple",
+        splitChunks: {
+            chunks: "all",
+            cacheGroups: {
+                chakra: {
+                    name: "chakra",
+                    test: /[\\/]node_modules[\\/]@chakra-ui[\\/]/,
+                    enforce: true,
+                    chunks: "initial",
+                },
+                framer: {
+                    name: "framer",
+                    test: /[\\/]node_modules[\\/]framer-motion[\\/]/,
+                    enforce: true,
+                    chunks: "initial",
+                },
+                react_dom: {
+                    name: "react-dom",
+                    test: /[\\/]node_modules[\\/]react-dom[\\/]/,
+                    enforce: true,
+                    chunks: "initial",
+                },
+                vendors: {
+                    name: "vendors",
+                    test: /[\\/]node_modules[\\/]/,
+                    enforce: true,
+                    chunks: "initial",
+                },
+                components: {
+                    name: "components",
+                    test: /[\\/]src[\\/]components[\\/]/,
+                    enforce: true
+                }
+            }
+        }
+    }
+};
+
+export default config;
