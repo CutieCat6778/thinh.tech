af7068d1626a81b39f59109e275a10b2f9a54d6f
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-07-02 16:04:38 +0200

    added turbo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import vercel from '@astrojs/vercel/static';
 import sitemap from "@astrojs/sitemap";
 import robots from "astro-robots-txt";
 
+import turbolinks from "@astrojs/turbolinks";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
@@ -11,19 +13,10 @@ export default defineConfig({
   experimental: {
     integrations: true
   },
-  integrations: [preact(), sitemap(
-    {
-      customPages: [
-        "https://www.thinh.tech/link",
-        "https://www.thinh.tech/project",
-        "https://www.thinh.tech/about",
-        "https://www.thinh.tech/404",
-        "https://www.thinh.tech/project/the-beginn",
-        "https://www.thinh.tech/project/moddy",
-      ]
-    }
-  ), robots({
+  integrations: [preact(), sitemap({
+    customPages: ["https://www.thinh.tech/link", "https://www.thinh.tech/project", "https://www.thinh.tech/about", "https://www.thinh.tech/404", "https://www.thinh.tech/project/the-beginn", "https://www.thinh.tech/project/moddy"]
+  }), robots({
     host: "www.thinh.tech",
-    sitemap: ['https://www.thinh.tech/sitemap.xml']
-  })]
+    sitemap: ['https://www.thinh.tech/sitemap-index.xml']
+  }), turbolinks()]
 });
