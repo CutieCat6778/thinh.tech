8ff0d8aa33c065af384393c1fe1eee604482b788
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2023-01-10 20:03:35 +0100

    fixed workbox

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
-const NextWorkboxWebpackPlugin = require('next-workbox-webpack-plugin');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+})
 
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, distDir }) => {
-    if (!isServer && !dev) {
-      config.plugins.push(
-        new NextWorkboxWebpackPlugin({
-          distDir,
-          buildId,
-          swDestRoot: './static/',
-          swURLRoot: '/static',
-          precacheManifest: true,
-        })
-      );
-    }
-
-    return config;
-  }
-}
+})
 
 module.exports = nextConfig
