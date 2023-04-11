c61bbcf5d0025f9df66010f5a7f08dbf3a75d6c4
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2023-04-11 17:08:24 +0200

    fixed sitemap

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = ({
   reactStrictMode: true,
-  siteUrl: process.env.SITE_URL || 'https://thinh.tech',
-  generateRobotsTxt: true,
 })
 
 module.exports = nextConfig
